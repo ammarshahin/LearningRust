0c6d08b4b77ef15a7471ef176cd658b0905f93f3
ammarshahin <ammar0ammar612@gmail.com>
2021-03-09 05:40:10 +0200

    init the repo

---
+++ src/print.rs
@@ -0,0 +1,8 @@
+
+pub fn run()
+{   
+    println!("Hello, world!");
+    println!("My name is {}", "ammar");
+    println!("My name is {} and I'm {}", "ammar", 26);
+    println!("My name is {0} and I'm {1}. {0} loves to code in rust", "ammar", 26);
+}
