d9b72fb8c7f8d7d92183dcb1b5d9ef8a6289a504
AmmarMShahin <Ammar-Shahin@elarabygroup.com>
2021-03-10 07:15:00 +0200

    adding the print.rs file

--- src/print.rs
+++ src/print.rs
@@ -5,4 +5,22 @@ pub fn run()
     println!("My name is {}", "ammar");
     println!("My name is {} and I'm {}", "ammar", 26);
     println!("My name is {0} and I'm {1}. {0} loves to code in rust", "ammar", 26);
+    
+
+    // Named Arguments
+  println!(
+    "{name} likes to play {activity}",
+    name = "ammar",
+    activity = "video Games"
+  );
+
+  // Placeholder traits
+  println!("Binary: {:b}, Hex: {:x}, Octal: {:o}", 10, 10, 10);
+
+  // Placeholder for debug trait
+  println!("{:?}", (12, true, "hello"));
+
+  // Basic math
+  println!("10 + 10 = {}", 10 + 10);
+
 }
