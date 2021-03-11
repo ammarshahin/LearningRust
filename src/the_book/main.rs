89a018290d55209b4dbf23e316282b8c77e28ce3
AmmarMShahin <Ammar-Shahin@elarabygroup.com>
2021-03-11 14:41:26 +0200

    adding read_line

--- src/the_book/main.rs
+++ src/the_book/main.rs
@@ -8,9 +8,12 @@ fn main() {
     let mut bar = 5; // mutable variable
     let mut guess = String::new(); // make a dynamically allocated variable guess(a mutable variable)
 
-    std::io::stdin() // read the input string from the stdin and put it into guess variable
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    std::io::stdin() // Read the input string from the stdin and put it into guess variable
+        .read_line(&mut guess) //  
+        .expect("Failed to read line"); // Handling Potential Failure with the Result Type
 
-    println!("You guessed: {}", guess); // {} is a placeholder that is the default.
+    // {:?} is the debug trait (the placeholder to print a full array)
+    // {0}, {1} ... is used to identify which parameters are used to print
+    // {} is a placeholder that is the default.
+    println!("You guessed: {}", guess);
 }
