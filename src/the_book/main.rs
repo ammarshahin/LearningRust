6c870898f9b095207a23f7f3e8f8ff9f7a495dae
AmmarMShahin <Ammar-Shahin@elarabygroup.com>
2021-03-31 10:46:14 +0200

    aaaa

--- src/the_book/main.rs
+++ src/the_book/main.rs
@@ -1,7 +1,11 @@
 // mod guessing_game_tutorial;
-mod var_and_mut;
+// mod var_and_mut;
+// mod ownership;
+mod references_and_borrowing;
 
 fn main() {
-   // guessing_game_tutorial::run();
-   var_and_mut::run();
+    // guessing_game_tutorial::run();
+    // var_and_mut::run();
+    //  ownership::run();
+    references_and_borrowing::run();
 }
