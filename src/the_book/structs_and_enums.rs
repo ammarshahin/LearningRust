bd40b65b842d1c01b09ccd8eebd71a76ad0fc25e
ammarshahin <ammar0ammar612@gmail.com>
2021-04-06 22:58:12 +0200

    finishing the struct chapter

--- src/the_book/structs_and_enums.rs
+++ src/the_book/structs_and_enums.rs
@@ -5,6 +5,7 @@ struct Color(i32, i32, i32);
 struct Point(f32, f32, f32);
 
 // Normal structs
+#[derive(Debug)] // the annotation #[derive(Debug)] is used to enable the debug print of the structure info
 struct User {
     name: String,
     email: String,
@@ -12,15 +13,18 @@ struct User {
     active: bool,
 }
 
+// implementation block.
 impl User {
     // constructor
+    // new is an associated functions (because it doesn't take &self as an parameter) and it's called with the >> StructName::associated_fun();
+    // the :: syntax is used for both associated functions and namespaces created by modules.
     //* Note: if the parameters name is the same as the fields name we can use it directly (like: sign_in_count, active)
     fn new(name: &str, email: &str, sign_in_count: u32, active: bool) -> Self {
         User {
             name: String::from(name),
             email: String::from(email),
             // Because the sign_in_count field and the sign_in_count parameter have the same name,
-            // we only need to write sign_in_count rather than sign_in_count: sign_in_count.
+            // we only need to write sign_in_count rather than sign_in_count: sign_in_count.....
             sign_in_count,
             // Same as the active field
             active,
@@ -30,7 +34,6 @@ impl User {
     fn get_name(&self) -> &str {
         &self.name
     }
-
     fn get_email(&self) -> &str {
         &self.email
     }
@@ -54,10 +57,11 @@ pub fn run() {
     // The syntax .. specifies that the remaining fields not explicitly set should have the same value as the fields in the given instance.
     let u3 = User {
         name: String::from("another Ammar Shahin"),
-        ..u2 // fill the remaining fields with the u2 struct
-    }; // But note now that the u2 has been borrowed here after partial move and can't now be used
+        ..u2 // fill the remaining fields with the corresponding u2 struct fields
+    }; // But note now that the u2 has been borrowed after partial move and can't be used anymore
 
     println!("the user email {}", u3.get_email());
+    println!("the user {:#?}", u3); // using the debug trait to print the struct info [simple = {:?} , detailed = {:#?}]
 
     // tuple structs
     let black = Color(10, 20, 30);
